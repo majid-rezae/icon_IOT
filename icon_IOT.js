31e1fde05df699f0b5ff0ff08fdc7465ddaaa11f
S. Majid Rezaee <88668826+majid-rezae@users.noreply.github.com>
2021-11-17 09:52:01 -0300

    Add files via upload

---
+++ icon_IOT.js
@@ -0,0 +1,12 @@
+
+import { Sunny, Cloudy, Rain, Snow } from 'weather-styled-icon';
+function Icon (){
+  return (
+    <div>
+      <Sunny />
+      <Cloudy />
+      <Rain />
+      <Snow />
+    </div>
+  );
+}
